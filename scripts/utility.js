8c4b09e65382bf258a19fed413ec3542f80dfb93
chinno@0007 <mdmehedi648382@gmail.com>
2024-02-19 00:15:33 -0600

    completed

--- scripts/utility.js
+++ scripts/utility.js
@@ -3,14 +3,14 @@ const allSeat = document.getElementsByClassName('seat');
 // console.log(allSeat);
 
 let count = 0;
+// let count2 =8;
 for(const seat of allSeat){
     seat.addEventListener('click', function(event){
         count = count+1;
 
         // seat number option
         const seatNumber = event.target.innerText;
-        
-
+        console.log(event.target);        
         // seat cost option
         const seatCategory = document.getElementById('seat-category');
         const div = document.createElement('div');
@@ -26,11 +26,52 @@ for(const seat of allSeat){
         div.appendChild(p);
         div.appendChild(p2);
         div.appendChild(p3)
+
+        event.target.disabled = true;
+        const totalSeatCount = document.getElementById("total-seat-quantity");
+        const totalSeat = totalSeatCount.innerText;
+        const seatLeft = parseInt(totalSeat);
+
+        const nowSeat = seatLeft -1;
+        
+        if(nowSeat<4){
+            alert('You can buy only 4 seat.');
+            return;
+        }
+        else{
+            
+            totalSeatCount.innerText = nowSeat;
+
+        }
+        // const convertedTotalSeatQuantity = parseInt(totalSeat);
+
+        // // if(convertedTotalSeatQuantity-count <4){
+        // //     alert("Maximum 4 Tickets can be bought by One Person");
+        // //     return;
+        // // }
+        // // document.getElementById("total-seat-quantity").innerText = parseInt(convertedTotalSeatQuantity) - parseInt(count);
+        // // // setInnerTextForRestOfTickets("total-seat-quantity", count)
+        // setInnerTextForRestOfTickets(convertedTotalSeatQuantity, 8)
+
+
+
+        // setBackgroundColorById(seatNumber);
+
+
+
+
+
+        event.target.style.backgroundColor = "gray"
+                // event.target.classList.add('disabled');
+
+
+
+
         seatCategory.appendChild(div);
 
 
         totalCost("total-price" , ticketPrice);
-
+        // hideElementsById(event.target);
 
         function NewGrandTotalCost(){
             const totalCost = document.getElementById("total-price").innerText;
@@ -64,10 +105,27 @@ for(const seat of allSeat){
             disableOption();
         }
     })
+    // disableButton(event.target)
+
         grandTotalCost("grand-total" , ticketPrice);
         setInnerText("seatQuantity", count);
     });
+
+
+
+    const submitForTickets = document.getElementById('next-submit');
+    submitForTickets.addEventListener('click', function(){
+
+        my_modal_4.showModal();
+
+    });
 }
+
+
+
+
+
+
 
 
 function totalCost(id, value){
@@ -89,8 +147,30 @@ function setInnerText(id, value){
     document.getElementById(id).innerText = value;
 
 }
+function setInnerTextForRestOfTickets(id, value){
+  const ticketsLeft=  document.getElementById(id).innerText = value;
+  ticketsLeft = value-1;
 
-function disableOption(){
+}
+
+function disableOption(id, value){
     const makingDisable = document.getElementById('coupon-taking-option');
     makingDisable.classList.add('hidden');
 }
+
+function setBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400');
+}
+
+
+function hideElementsById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden');
+}
+
+function showElementById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden');
+}
+
